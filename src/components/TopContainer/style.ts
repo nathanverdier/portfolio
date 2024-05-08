7de9dcc2089ceed991c931e0456a2916a21a5760
Nathan <nathanverdier34@gmail.com>
2024-05-08 10:57:27 +0200

    🚧 Add : margin-bottom to TopContainer component

--- src/components/TopContainer/style.ts
+++ src/components/TopContainer/style.ts
@@ -7,6 +7,7 @@ export const Conteneur = styled.div`
     align-items: center;
     justify-content: center;
     position: relative;
+    margin-bottom: 50px;
 `;
 
 export const StyledImage = styled.img`
