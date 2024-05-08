dbd22332c4b75b4e339d8dec961dd2e5748dd2d3
Nathan <nathanverdier34@gmail.com>
2024-05-08 11:08:56 +0200

    🚧 Refactor : Navbar component to update links in Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     return(
         <StyledTabNavRoot justify="start" wrap="wrap">
             <TabNav.Link href="#" active>Accueil</TabNav.Link>
-            <TabNav.Link href="#">A propos de moi</TabNav.Link>
-            <TabNav.Link href="#">CV</TabNav.Link>
-            <TabNav.Link href="#">Mes projets</TabNav.Link>
+            <TabNav.Link href="#apropos">A propos de moi</TabNav.Link>
+            <TabNav.Link href="#cv">CV</TabNav.Link>
+            <TabNav.Link href="#projects">Mes projets</TabNav.Link>
         </StyledTabNavRoot>
       
     );
