2f2b968078ec9f542f040fc4c7809f1e35c1e7e9
Nathan <nathanverdier34@gmail.com>
2024-05-08 11:11:45 +0200

    🚧 Refactor : Navbar component to update active tab behavior in Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,17 @@
 import { TabNav } from "@radix-ui/themes";
 import { StyledTabNavRoot } from "./style";
+import { useState } from 'react';
 
 const Navbar = () => {
-    
+
+    const [activeTab, setActiveTab] = useState('accueil');
+
     return(
         <StyledTabNavRoot justify="start" wrap="wrap">
-            <TabNav.Link href="#" active>Accueil</TabNav.Link>
-            <TabNav.Link href="#apropos">A propos de moi</TabNav.Link>
-            <TabNav.Link href="#cv">CV</TabNav.Link>
-            <TabNav.Link href="#projects">Mes projets</TabNav.Link>
+            <TabNav.Link href="#" active={activeTab === 'accueil'} onClick={() => setActiveTab('accueil')}>Accueil</TabNav.Link>
+            <TabNav.Link href="#apropos" active={activeTab === 'apropos'} onClick={() => setActiveTab('apropos')}>A propos de moi</TabNav.Link>
+            <TabNav.Link href="#cv" active={activeTab === 'cv'} onClick={() => setActiveTab('cv')}>CV</TabNav.Link>
+            <TabNav.Link href="#projets" active={activeTab === 'projets'} onClick={() => setActiveTab('projets')}>Mes projets</TabNav.Link>
         </StyledTabNavRoot>
       
     );
