ec44ec462dcc0efd3b1d7acf2d2b51fac073c457
Nathan <nathanverdier34@gmail.com>
2024-05-15 12:09:32 +0200

    🚧 Add : project data for Project1, Project2, and Project3

---
+++ src/utils/projectData.ts
@@ -0,0 +1,20 @@
+export const project1Data = {
+    titre: "Project1",
+    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
+    date: "Date du CV",
+    description: "Description du CV ezc ezc ze cezc ez c",
+};
+
+export const project2Data = {
+    titre: "Project2",
+    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
+    date: "Date du CV",
+    description: "Description du CV ezc ezc ze cezc ez c",
+};
+
+export const project3Data = {
+    titre: "Project3",
+    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
+    date: "Date du CV",
+    description: "Description du CV ezc ezc ze cezc ez c",
+};
