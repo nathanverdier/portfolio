e6b15009277c8832d1b9d3193a5512e0678fdc7b
Nathan <nathanverdier34@gmail.com>
2024-06-25 15:55:58 +0200

    ✨ feat: Update CvContainer to include date prop

--- src/components/CvContainer/CvContainer.tsx
+++ src/components/CvContainer/CvContainer.tsx
@@ -7,21 +7,24 @@ interface CvContainerProps {
 	description : string;
 }
 
-const CvContainer = ({imagesSource, titre, description} : CvContainerProps) => {
+const CvContainer = ({imagesSource, titre, date,  description} : CvContainerProps) => {
     
     return(
-        <Box maxWidth="240px">
-        <Card>
+        <Flex style={{ height: '140px', alignItems: 'center', justifyContent: 'center' }}>
+        <Card style={{ width: '800px' }}>
             <Flex gap="3" align="center">
             <Avatar
-                size="5"
+                size="6"
                 src={imagesSource}
                 radius="full"
                 fallback="T"
             />
             <Box>
-                <Text as="div" size="2" weight="bold">
+                <Text as="div" size="3" weight="bold">
                 {titre}
+                </Text>
+                <Text as="div" size="2" weight="bold">
+                {date}
                 </Text>
                 <Text as="div" size="2" color="gray">
                 {description}
@@ -29,7 +32,7 @@ const CvContainer = ({imagesSource, titre, description} : CvContainerProps) => {
             </Box>
             </Flex>
         </Card>
-        </Box>
+        </Flex>
 
 // "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
       
