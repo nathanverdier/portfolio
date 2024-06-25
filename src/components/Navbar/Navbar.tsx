880b5ad97a9bbff54f48a34d02c0e62e4e6674c9
Nathan <nathanverdier34@gmail.com>
2024-06-25 15:56:12 +0200

    ✨ feat: Add lazy loading for images in Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
 import { TabNav } from "@radix-ui/themes";
 import { StyledTabNavRoot } from "./style";
-import { useState } from 'react';
 
 const Navbar = () => {
-
     const [activeTab, setActiveTab] = useState('accueil');
+    const sectionsRef = useRef<{ [key: string]: HTMLElement | null }>({});
 
     const scrollTo = (elementId: string) => {
-        document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
-      };
+        const element = document.getElementById(elementId);
+        if (element) {
+            const offset = elementId === 'accueil' ? 0 : 50; // Pas de marge pour 'home'
+            window.scrollTo({
+                top: element.offsetTop - offset,
+                behavior: 'smooth'
+            });
+        }
+    };
 
     const scrollToTop = () => {
         window.scrollTo({
-            top: 0,
+            top: 50,
             behavior: 'smooth'
         });
     };
 
-    return(
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach(entry => {
+                    if (entry.boundingClientRect.top < 0) {
+                        setActiveTab(entry.target.id);
+                    }
+                });
+            },
+            { threshold: 0 }
+        );
+
+        const sections = ['accueil', 'apropos', 'cv', 'projets'];
+        sections.forEach(id => {
+            const element = document.getElementById(id);
+            if (element) {
+                sectionsRef.current[id] = element;
+                observer.observe(element);
+            }
+        });
+
+        return () => {
+            if (sectionsRef.current) {
+                sections.forEach(id => {
+                    const element = sectionsRef.current[id];
+                    if (element) {
+                        observer.unobserve(element);
+                    }
+                });
+            }
+        };
+    }, []);
+
+    return (
         <StyledTabNavRoot justify="start" wrap="wrap">
             <TabNav.Link onClick={() => { setActiveTab('accueil'); scrollToTop(); }} active={activeTab === 'accueil'}>Accueil</TabNav.Link>
             <TabNav.Link onClick={() => { setActiveTab('apropos'); scrollTo('apropos'); }} active={activeTab === 'apropos'}>A propos de moi</TabNav.Link>
