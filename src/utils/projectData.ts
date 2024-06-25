814b917b537b8b7d9b435bb5c59a34e398118bea
Nathan <nathanverdier34@gmail.com>
2024-06-25 15:56:41 +0200

    ✨ feat: Update project data for Project1, Project2, and Project3

--- src/utils/projectData.ts
+++ src/utils/projectData.ts
@@ -1,20 +1,23 @@
+import kotangaLogo from '../assets/kotanga.png';
+import DouShouQiLogo from '../assets/doushouqi.png';
+
 export const project1Data = {
-    titre: "Project1",
-    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
-    date: "Date du CV",
-    description: "Description du CV ezc ezc ze cezc ez c",
+    titre: "Dou Shou Qi",
+    imagesSource: DouShouQiLogo,
+    date: "24/06/2024",
+    description: "Project réaliser en Swift",
 };
 
 export const project2Data = {
-    titre: "Project2",
+    titre: "Project3",
     imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
     date: "Date du CV",
     description: "Description du CV ezc ezc ze cezc ez c",
 };
 
 export const project3Data = {
-    titre: "Project3",
-    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
-    date: "Date du CV",
-    description: "Description du CV ezc ezc ze cezc ez c",
+    titre: "Kotanga",
+    imagesSource: kotangaLogo,
+    date: "29/04/2023",
+    description: "Project réaliser en Kotlin",
 };
