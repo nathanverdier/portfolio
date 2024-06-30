f98ab5e3d6fab6ef9f468d306efaf75d30c6c95b
Nathan <nathanverdier34@gmail.com>
2024-06-30 20:15:39 +0200

    ✨ feat: Update ProjetPopup component to include description and text props

--- src/components/ProjetPopup/ProjetPopup.tsx
+++ src/components/ProjetPopup/ProjetPopup.tsx
@@ -1,39 +1,55 @@
 import { Button, Dialog, Flex, Text } from "@radix-ui/themes";
-import testGif from '../../assets/testGif.gif';
 
+interface ProjetProps {
+    data: {
+        imagesSource: string;
+        titre: string;
+        date: string;
+        descriptionLongue: string;
+        text: string;
+        url: string;
+    } | null; 
+    onClick: () => void;
+}
 
-const ProjetPopup = ({ onClose, title, description, text }: { onClose: () => void, title: string, description: string, text: string }) => {
+const ProjetPopup = ({ data, onClick }: ProjetProps) => {
+    if (!data) {
+        return null;
+    }
 
-    return(
+    const { imagesSource, titre, descriptionLongue, text, url } = data;
+
+    return (
         <Dialog.Root open={true}>
+            <Dialog.Content maxWidth="850px" style={{ position: 'relative' }}>
+                <Dialog.Close onClick={onClick} style={{ position: 'absolute', top: '20px', right: '20px' }}>        
+                    <Button variant="soft" color="red">
+                        Fermer
+                    </Button>
+                </Dialog.Close> 
+                <Dialog.Title>{titre}</Dialog.Title>
+                <Dialog.Description size="2" mb="4">
+                    {descriptionLongue} 
+                </Dialog.Description>
 
-        <Dialog.Content maxWidth="850px" style={{ position: 'relative' }}>
-            <Dialog.Close onClick={onClose} style={{ position: 'absolute', top: '20px', right: '20px' }}>        
-                <Button variant="soft" color="red">
-                    Fermer
-                </Button>
-            </Dialog.Close> 
-            <Dialog.Title>{title}</Dialog.Title>
-            <Dialog.Description size="2" mb="4">
-            {description} 
-            </Dialog.Description>
+                <Flex direction="column" gap="3">
+                    <img src={imagesSource} alt="Description" />
+                    <label>
+                        <Text as="div" size="2" mb="1" weight="bold">
+                            {text} 
+                        </Text>
+                    </label>
+                </Flex>
 
-            <Flex direction="column" gap="3">
-            <img src={testGif} alt="Description" />
-            <label>
-                <Text as="div" size="2" mb="1" weight="bold">
-                {text} 
-                </Text>
-            </label>
-            </Flex>
-
-            <Flex gap="3" mt="4" justify="end">
-            
-            </Flex>
-        </Dialog.Content>
+                <Flex gap="3" mt="4" justify="center">
+                    <a href={url} style={{ textDecoration: 'none' }}>
+                        <Button variant="ghost">
+                            En savoir plus
+                        </Button>
+                    </a>
+                </Flex>
+            </Dialog.Content>
         </Dialog.Root>
-
-
     );
 };
 
