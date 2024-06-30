21b60a6b71185cecfcf625ce9d20216cee8dcbe1
Nathan <nathanverdier34@gmail.com>
2024-06-30 20:15:27 +0200

    ✨ feat: Add images and update project data - Add new images for the projects - Update project data to include 'imageslogo' property instead of 'imagesSource'

--- src/utils/projectData.ts
+++ src/utils/projectData.ts
@@ -1,23 +1,39 @@
 import kotangaLogo from '../assets/kotanga.png';
 import DouShouQiLogo from '../assets/doushouqi.png';
+import DouShouQiImg from '../assets/imageDoushouQI.png';
+import BoardConquestLogo from '../assets/logoBoardConquest.png';
+import BoardConquestImg from '../assets/boardConquest.png';
 
 export const project1Data = {
     titre: "Dou Shou Qi",
-    imagesSource: DouShouQiLogo,
+    imageslogo: DouShouQiLogo,
+    imagesSource: DouShouQiImg,
     date: "24/06/2024",
     description: "Project réaliser en Swift",
+    descriptionLongue: "Une application iOS du jeu de société DouShouQi",
+    text: "Swift, SwiftUI, SpriteKit. Réalisée au sein d'une équipe de trois, durant ma 3ème année de BUT, l'application permet de jouer au jeu de société chinois DouShouQi. Elle permet aussi de voir la liste des joueurs que vous avez créés ainsi que leur score respectif. L'application a été développé en Swift et utilise les bibliothèques SpriteKit et SwiftUI.",
+    url: "https://codefirst.iut.uca.fr/git/remi.regnault/DouShouQi_App"
 };
 
 export const project2Data = {
-    titre: "Project3",
-    imagesSource: "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop",
-    date: "Date du CV",
-    description: "Description du CV ezc ezc ze cezc ez c",
+    titre: "BoardConquest",
+    imageslogo: BoardConquestLogo,
+    imagesSource: BoardConquestImg,
+    date: "23/06/23",
+    description: "Unreal Engine, C++",
+    descriptionLongue: "Projet réaliser au sein de l'UQAC en C++ et Unreal Engine",
+    text: "Réalisée au sein d'une équipe de 2, durant ma formation au sein de l'UQAC, le jeu est un jeu d'échec 3D avec des pions de différentes races. Vous pouvez choisir entre 3 plateaux de jeu dont la taille et les pions de départ varient, mais aussi 3 pouvoir à utiliser intelligement durant votre partie. L'application a été développé avec Unreal Engine et pour la majeur partie des fonctionnalités, en C++.",
+    url: "https://gitlab.com/nathanverdier/boardconquest"
 };
 
 export const project3Data = {
     titre: "Kotanga",
-    imagesSource: kotangaLogo,
+    imageslogo: kotangaLogo,
+    magesSource: kotangaLogo,
     date: "29/04/2023",
     description: "Project réaliser en Kotlin",
+    descriptionLongue: "Project réaliser en Swift",
+    text: "Notre application porte pour nom Kotanga ce qui signifie compter en Lingala. Réalisé en équipe de 5, elle a pour but de faciliter les paiments entre amis, au sein d'un groupe de personne comme des collocataires, des familles, ...Sur notre application, vous serez averti dès qu'un autre utilisateur d'un même groupe que vous envoie un message, actualise ses dettes, déclare une nouvelle dépense et autre... Le scanner vous permettra un gain de temps lorsque vous souhaitez déclarer une nouvelle dépense. L'application est totalement gratuite et sans pub ! Vos données ne sont accessibles qu'à vous et vous seul et sont stockées sur une base de données complétement sécurisée.",
+    url: "https://gitlab.com/nathanverdier/kotanga"
 };
+
