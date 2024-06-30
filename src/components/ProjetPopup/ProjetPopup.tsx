0206d8827f50fe635c7da1b60ba718514d8b230d
Nathan <nathanverdier34@gmail.com>
2024-06-30 19:09:48 +0200

    🔥 Update ProjetPopup component to include description and text props

--- src/components/ProjetPopup/ProjetPopup.tsx
+++ src/components/ProjetPopup/ProjetPopup.tsx
@@ -1,7 +1,8 @@
 import { Button, Dialog, Flex, Text } from "@radix-ui/themes";
 import testGif from '../../assets/testGif.gif';
 
-const ProjetPopup = ({ onClose, title }: { onClose: () => void, title: string }) => {
+
+const ProjetPopup = ({ onClose, title, description, text }: { onClose: () => void, title: string, description: string, text: string }) => {
 
     return(
         <Dialog.Root open={true}>
@@ -14,20 +15,14 @@ const ProjetPopup = ({ onClose, title }: { onClose: () => void, title: string })
             </Dialog.Close> 
             <Dialog.Title>{title}</Dialog.Title>
             <Dialog.Description size="2" mb="4">
-            Jeux de société
+            {description} 
             </Dialog.Description>
 
             <Flex direction="column" gap="3">
             <img src={testGif} alt="Description" />
             <label>
                 <Text as="div" size="2" mb="1" weight="bold">
-                Inter has ruinarum varietates a Nisibi quam tuebatur accitus Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.
-
-                Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
-
-                Coactique aliquotiens nostri pedites ad eos persequendos scandere clivos sublimes etiam si lapsantibus plantis fruticeta prensando vel dumos ad vertices venerint summos, inter arta tamen et invia nullas acies explicare permissi nec firmare nisu valido gressus: hoste discursatore rupium abscisa volvente, ruinis ponderum inmanium consternuntur, aut ex necessitate ultima fortiter dimicante, superati periculose per prona discedunt.
-
-
+                {text} 
                 </Text>
             </label>
             </Flex>
