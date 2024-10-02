3021f14652e494c85b280540c14e36e0477aa2b1
Nathan <nathanverdier34@gmail.com>
2024-10-02 19:48:25 +0200

    ♻️ Refactor: Update APropos component content

--- src/components/APropos/APropos.tsx
+++ src/components/APropos/APropos.tsx
@@ -14,38 +14,36 @@ const APropos = () => {
                     <MyAvatar imageUrl={myPicture} altText="Photo de profil" fallbackText="Photo de profil" />
                 </div>
                      <CustomContainerText>
-                        <StyledText> Bonjour ! </StyledText>
-                        <StyledText>Je suis actuellement en train de terminer ma 3ème année de BUT Informatique en alternance chez Imaaagery. Cette expérience m'a permis de plonger au cœur du monde professionnel tout en approfondissant mes connaissances dans le domaine de l'informatique.</StyledText>
-                        <StyledText> En dehors de mes études et de mon travail en alternance, je suis passionné par tout ce qui touche aux systèmes embarqués, aux langages de programmation de bas et haut niveau, au système UNIX/Linux et à la cybersécurité.</StyledText>
-                        <StyledText> Je consacre également beaucoup de temps à la musculation et à maintenir un bien-être personnel. Je pratique régulièrement une activité physique en salle de musculation et je prône le bien-être, tant physique que mental. </StyledText>
-
-                        </CustomContainerText>
-                        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                            <MyAvatar imageUrl={logoEtud} altText="Photo de profil" fallbackText="Photo de profil" />
-                            <StyledH2 style={{ marginLeft: '20px' }}>Mon parcours scolaire</StyledH2>
-                        </div>
+                        <StyledText>Hello!</StyledText>
+                        <StyledText>I am currently finishing my 3rd year of a Bachelor's degree in Computer Science through an apprenticeship at Imaaagery. This experience has allowed me to dive into the professional world while deepening my knowledge in the field of computer science.</StyledText>
+                        <StyledText>Outside of my studies and apprenticeship work, I am passionate about everything related to embedded systems, low-level and high-level programming languages, UNIX/Linux systems, and cybersecurity.</StyledText>
+                        <StyledText>I also devote a lot of time to bodybuilding and maintaining personal well-being. I regularly engage in physical activity at the gym and advocate for well-being, both physical and mental.</StyledText>
+                    </CustomContainerText>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <MyAvatar imageUrl={logoEtud} altText="Profile photo" fallbackText="Profile photo" />
+                        <StyledH2 style={{ marginLeft: '20px' }}>My Educational Background</StyledH2>
+                    </div>
                     <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', gap: '20px' }}>
-                    <CustomContainerText2>
-                        <StyledText style={{ fontWeight: 'bold' }}>BACCALAUREAT STI2D</StyledText><br/>
-                        <StyledText>septembre 2018 - juin 2020</StyledText><br />
-                        <StyledText>Lycée Jean-Zay, Thiers (63)</StyledText><br />
-                        <StyledText>Mention assez bien.</StyledText><br />
-                    </CustomContainerText2>
-                    <CustomContainerText2>
-                        <StyledText style={{ fontWeight: 'bold' }}>Diplôme Informatique, 2 cycle</StyledText><br />
-                        <StyledText>janvier 2023 - juillet 2023</StyledText><br />
-                        <StyledText>UQAC, Chicoutimi (Canada G7H 2B1)</StyledText><br />
-                        <StyledText>Programme court de 1er cycle en informatique pour étudiant en séjour d'études.</StyledText><br />
-                    </CustomContainerText2>
+                        <CustomContainerText2>
+                            <StyledText style={{ fontWeight: 'bold' }}>HIGH SCHOOL DIPLOMA (STI2D)</StyledText><br />
+                            <StyledText>September 2018 - June 2020</StyledText><br />
+                            <StyledText>Jean-Zay High School, Thiers (63)</StyledText><br />
+                            <StyledText>Graduated with honors.</StyledText><br />
+                        </CustomContainerText2>
+                        <CustomContainerText2>
+                            <StyledText style={{ fontWeight: 'bold' }}>Computer Science Diploma, 2nd Cycle</StyledText><br />
+                            <StyledText>January 2023 - July 2023</StyledText><br />
+                            <StyledText>UQAC, Chicoutimi (Canada G7H 2B1)</StyledText><br />
+                            <StyledText>Short program for undergraduate students on study abroad.</StyledText><br />
+                        </CustomContainerText2>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
-                    <CustomContainerText2>
-                        <StyledText style={{ fontWeight: 'bold' }}>Bachelor Universitaire de Technologie Informatique </StyledText><br />
-                        <StyledText>septembre 2021 - Septembre 2024</StyledText><br />
-                        <StyledText>IUT Informatique, Clermont-Ferrand (63)</StyledText><br />
-                        <StyledText>Diplôme Universitaire de Technologie Informatique.
-                            Lauréat du prix “Numérique responsable” lors de l’évènement “24h pour entreprendre”.</StyledText><br />
-                    </CustomContainerText2>
+                        <CustomContainerText2>
+                            <StyledText style={{ fontWeight: 'bold' }}>University Bachelor of Technology in Computer Science</StyledText><br />
+                            <StyledText>September 2021 - September 2024</StyledText><br />
+                            <StyledText>IUT Informatique, Clermont-Ferrand (63)</StyledText><br />
+                            <StyledText>University Diploma of Technology in Computer Science. Awarded the "Responsible Digital" prize during the "24h to undertake" event.</StyledText><br />
+                        </CustomContainerText2>
                     </div>
             </CustomContainer>
         </Box>
