9af2d18bd1b90869a8c3bc7f64a5be700ddc1dd0
Nathan <nathanverdier34@gmail.com>
2024-10-04 01:52:54 +0200

    🐛 Refactor: Update GitHub profile link in Competence component

--- src/components/Competence/Competence.tsx
+++ src/components/Competence/Competence.tsx
@@ -10,7 +10,7 @@ const Competence = () => {
             <TabNav.Link href="#" > <BsFillTelephoneInboundFill/>: 07 81 14 55 43</TabNav.Link>
             <TabNav.Link href="mailto:nathanverdier34@gmail.com"> <EnvelopeOpenIcon/> : nathanverdier34@gmail.com </TabNav.Link>
             <TabNav.Link href="https://www.linkedin.com/in/nathan-verdier-1b6421116/"> <LinkedInLogoIcon/> : nathan-verdier </TabNav.Link>
-            <TabNav.Link href="https://github.com/waarx11"> <GitHubLogoIcon/> : My projects</TabNav.Link>
+            <TabNav.Link href="https://github.com/nathanverdier"> <GitHubLogoIcon/> : My projects</TabNav.Link>
             <TabNav.Link href="#"> <DownloadIcon/> : My resume</TabNav.Link>
         </StyledTabNavRoot>
       
