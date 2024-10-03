6ba11b84f76170a557b23b35b5016665f1e81395
Nathan <nathanverdier34@gmail.com>
2024-10-04 01:52:41 +0200

    🐛 Refactor: Update links in BarBottom component

--- src/components/BarBottom/BarBottom.tsx
+++ src/components/BarBottom/BarBottom.tsx
@@ -10,8 +10,8 @@ const BarBottom = () => {
             <TabNav.Link href="#" style={{ pointerEvents: "none" }}> <BsFillTelephoneInboundFill/>: 07 81 14 55 43</TabNav.Link>
             <TabNav.Link href="mailto:nathanverdier34@gmail.com"> <EnvelopeOpenIcon/> : nathanverdier34@gmail.com </TabNav.Link>
             <TabNav.Link href="https://www.linkedin.com/in/nathan-verdier-1b6421116/"> <LinkedInLogoIcon/> : nathan-verdier </TabNav.Link>
-            <TabNav.Link href="https://github.com/waarx11"> <GitHubLogoIcon/> : My projects</TabNav.Link>
-            <TabNav.Link href="https://waarx11.github.io/portfolio/NATHAN_VERDIER_CV.pdf"> <DownloadIcon/> : My resume</TabNav.Link>
+            <TabNav.Link href="https://github.com/nathanverdier"> <GitHubLogoIcon/> : My projects</TabNav.Link>
+            <TabNav.Link href="https://nathanverdier.github.io/portfolio/NATHAN_VERDIER_CV.pdf"> <DownloadIcon/> : My resume</TabNav.Link>
         </StyledTabNavRoot>
       
     );
