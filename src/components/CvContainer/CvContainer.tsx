805732d7b8d37d1fb313b94f1c7e482a54c0581d
Nathan <nathanverdier34@gmail.com>
2024-12-13 20:44:29 +0100

    💄 Style: Refactor CvContainer component to use styled components for improved readability and maintainability

--- src/components/CvContainer/CvContainer.tsx
+++ src/components/CvContainer/CvContainer.tsx
@@ -1,41 +1,38 @@
-import { Avatar, Box, Card, Flex, Text } from "@radix-ui/themes";
+import { Avatar, Box, Flex } from "@radix-ui/themes";
+import { StyledFlex, StyledCard, StyledText } from "./style";
 
 interface CvContainerProps {
-	imagesSource : string;
-	titre : string;
-	date : string;
-	description : string;
+    imagesSource: string;
+    titre: string;
+    date: string;
+    description: string;
 }
 
-const CvContainer = ({imagesSource, titre, date,  description} : CvContainerProps) => {
-    
-    return(
-        <Flex style={{ height: '140px', alignItems: 'center', justifyContent: 'center' }}>
-        <Card style={{ width: '800px' }}>
-            <Flex gap="3" align="center">
-            <Avatar
-                size="6"
-                src={imagesSource}
-                radius="full"
-                fallback="T"
-            />
-            <Box>
-                <Text as="div" size="3" weight="bold">
-                {titre}
-                </Text>
-                <Text as="div" size="2" weight="bold">
-                {date}
-                </Text>
-                <Text as="div" size="2" color="gray">
-                {description}
-                </Text>
-            </Box>
-            </Flex>
-        </Card>
-        </Flex>
-
-// "https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-      
+const CvContainer = ({ imagesSource, titre, date, description }: CvContainerProps) => {
+    return (
+        <StyledFlex>
+            <StyledCard>
+                <Flex gap="3" align="center">
+                    <Avatar
+                        size="6"
+                        src={imagesSource}
+                        radius="full"
+                        fallback="T"
+                    />
+                    <Box>
+                        <StyledText as="div" size="3" bold>
+                            {titre}
+                        </StyledText>
+                        <StyledText as="div" size="2" bold>
+                            {date}
+                        </StyledText>
+                        <StyledText as="div" size="2" color="gray">
+                            {description}
+                        </StyledText>
+                    </Box>
+                </Flex>
+            </StyledCard>
+        </StyledFlex>
     );
 };
 
