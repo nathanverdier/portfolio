468fe91780151d5dea3e3d741a7acf307da3a54d
Nathan <nathanverdier34@gmail.com>
2024-12-13 21:41:20 +0100

    💄 Style: Refactor MesProjectsContainer styles with responsive adjustments and new StyledCard and StyledAvatar components

--- src/components/MesProjectsContainer/style.ts
+++ src/components/MesProjectsContainer/style.ts
@@ -1,50 +1,50 @@
 import { styled } from "styled-components";
-import { Box, Container, Text } from "@radix-ui/themes";
-
-export const StyledH2 = styled.h2`
-    text-align: center;
-    line-height: 1.5; /* Line-height par défaut */
-
-    @media (max-width: 600px) {
-        font-size: 2rem; /* Augmentez légèrement la taille de la police pour les petits écrans */
-        line-height: 1.1; /* Réduisez encore plus l'interligne pour les petits écrans */
-    }
-`;
-
-export const StyledText = styled(Text)`
-    text-align: left;
-
-    @media (max-width: 600px) {
-        font-size: 0.9rem; /* Réduisez la taille de la police pour les petits écrans */
-    }
-`;
+import { Avatar, Box, Card, Container, Text } from "@radix-ui/themes";
 
 export const CustomContainer = styled(Container)`
     width: 100%;
     max-width: 100%;
+    margin-bottom: 50px;
 
     @media (max-width: 600px) {
-        padding: 10px; /* Réduisez le padding pour les petits écrans */
+        margin-bottom: 40px; /* Réduisez légèrement la marge pour les petits écrans */
     }
 `;
 
 export const CustomContainerText = styled(Container)`
     width: 100%;
     max-width: 100%;
+    background-color: lightgray;
     padding: 20px;
+    border-radius: 50px;
 
     @media (max-width: 600px) {
-        padding: 15px; /* Réduisez le padding pour les petits écrans */
+        padding: 15px; /* Réduisez légèrement le padding pour les petits écrans */
+        border-radius: 45px; /* Réduisez légèrement le rayon de bordure pour les petits écrans */
     }
 `;
 
-export const CustomBox = styled(Box)`
-    background: blue;
-    border-radius: var(--radius-3);
-    width: 100%;
-    max-width: 100%;
+export const StyledCard = styled(Card)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 
     @media (max-width: 600px) {
-        padding: 10px; /* Ajoutez du padding pour les petits écrans */
+        padding: 10px; /* Réduisez le padding pour les petits écrans */
+    }
+`;
+
+export const StyledAvatar = styled(Avatar)`
+    @media (max-width: 600px) {
+        width: 40px; /* Réduisez la largeur de l'avatar pour les petits écrans */
+        height: 40px; /* Réduisez la hauteur de l'avatar pour les petits écrans */
+    }
+`;
+
+export const StyledText = styled(Text)`
+    @media (max-width: 600px) {
+        font-size: 0.9rem; /* Réduisez légèrement la taille de la police pour les petits écrans */
     }
 `;
