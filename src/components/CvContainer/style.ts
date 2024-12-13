a58707522dc3153f1db06982ea26a91010a10b20
Nathan <nathanverdier34@gmail.com>
2024-12-13 20:44:33 +0100

    💄 Style: Add styled components for CvContainer to enhance responsiveness and maintainability

---
+++ src/components/CvContainer/style.ts
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+import { Card, Flex, Text } from "@radix-ui/themes";
+
+interface StyledTextProps {
+    bold?: boolean;
+}
+
+export const StyledFlex = styled(Flex)`
+    height: 140px;
+    align-items: center;
+    justify-content: center;
+
+    @media (max-width: 600px) {
+        height: auto; /* Ajustez la hauteur pour les petits écrans */
+        padding: 10px; /* Ajoutez du padding pour les petits écrans */
+    }
+`;
+
+export const StyledCard = styled(Card)`
+    width: 800px;
+
+    @media (max-width: 600px) {
+        width: 100%; /* Utilisez 100% de la largeur pour les petits écrans */
+        padding: 10px; /* Ajoutez du padding pour les petits écrans */
+    }
+`;
+
+export const StyledText = styled(Text)<StyledTextProps>`
+    font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')}; /* Déterminez le poids de la police en fonction de la prop */
+
+    @media (max-width: 600px) {
+        font-size: 0.9rem; /* Réduisez légèrement la taille de la police pour les petits écrans */
+    }
+`;
