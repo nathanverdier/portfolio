aef8e30c1c27cd5eeb93b48660db48781d0ba636
Nathan <nathanverdier34@gmail.com>
2024-12-13 19:50:29 +0100

    💄 Style: Reduce padding in MainContainer and MainContent for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,43 @@ import APropos from './components/APropos/APropos';
 import { MesProjects } from './components/MesProjects';
 
 function App() {
-  
-	return (
-		<MainContainer>
-			<Navbar/>
-			<MainContent>
-				<TopContainer/>
-				<APropos/>
-				<CVBars/>
-				<MesProjects/>
-			</MainContent>
-			{/* <ToastContainer /> */}
-			<BarBottom/>
-		</MainContainer>
-	);
+    return (
+        <MainContainer>
+            <Navbar/>
+            <MainContent>
+                <TopContainer/>
+                <APropos/>
+                <CVBars/>
+                <MesProjects/>
+            </MainContent>
+            <BarBottom/>
+        </MainContainer>
+    );
 }
 
 const MainContainer = styled.div`
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-	align-items: center;
-	width: 100%;
-	height: 100%;
-	padding-top: 64px;
-	padding-bottom: 64px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding-top: 32px; /* Réduisez encore plus le padding */
+    padding-bottom: 32px; /* Réduisez encore plus le padding */
+    box-sizing: border-box;
 `;
 
 const MainContent = styled.div`
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-	align-items: center;
-	width: 100%;
-	height: 100%;
-	padding: 16px;
-	overflow-y: auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    max-width: 1280px; /* Appliquez une largeur maximale pour les écrans plus larges */
+    padding: 8px; /* Réduisez encore plus le padding */
+    box-sizing: border-box;
 `;
 
 export default App
