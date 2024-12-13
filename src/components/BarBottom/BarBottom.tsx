8ad9af2294f07a708890745b495ad4e9f6377f6d
Nathan <nathanverdier34@gmail.com>
2024-12-13 20:44:15 +0100

    💄 Style: Simplify BarBottom component by removing inline styles for improved readability and maintainability

--- src/components/BarBottom/BarBottom.tsx
+++ src/components/BarBottom/BarBottom.tsx
@@ -5,20 +5,20 @@ import { GitHubLogoIcon, DownloadIcon, LinkedInLogoIcon, EnvelopeOpenIcon } from
 
 const BarBottom = () => {
     return (
-        <StyledTabNavRoot style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', width: '100%', boxSizing: 'border-box' }}>
-            <TabNav.Link href="#" style={{ pointerEvents: "none", flex: '1 1 200px', textAlign: 'center', boxSizing: 'border-box' }}>
+        <StyledTabNavRoot>
+            <TabNav.Link href="#" style={{ pointerEvents: "none" }}>
                 <BsFillTelephoneInboundFill />: 07 81 14 55 43
             </TabNav.Link>
-            <TabNav.Link href="mailto:nathanverdier34@gmail.com" style={{ flex: '1 1 200px', textAlign: 'center', boxSizing: 'border-box' }}>
+            <TabNav.Link href="mailto:nathanverdier34@gmail.com">
                 <EnvelopeOpenIcon /> : nathanverdier34@gmail.com
             </TabNav.Link>
-            <TabNav.Link href="https://www.linkedin.com/in/nathan-verdier-1b6421116/" style={{ flex: '1 1 200px', textAlign: 'center', boxSizing: 'border-box' }}>
+            <TabNav.Link href="https://www.linkedin.com/in/nathan-verdier-1b6421116/">
                 <LinkedInLogoIcon /> : nathan-verdier
             </TabNav.Link>
-            <TabNav.Link href="https://github.com/nathanverdier" style={{ flex: '1 1 200px', textAlign: 'center', boxSizing: 'border-box' }}>
+            <TabNav.Link href="https://github.com/nathanverdier">
                 <GitHubLogoIcon /> : My projects
             </TabNav.Link>
-            <TabNav.Link href="https://nathanverdier.github.io/portfolio/NATHAN_VERDIER_CV.pdf" style={{ flex: '1 1 200px', textAlign: 'center', boxSizing: 'border-box' }}>
+            <TabNav.Link href="https://nathanverdier.github.io/portfolio/NATHAN_VERDIER_CV.pdf">
                 <DownloadIcon /> : My resume
             </TabNav.Link>
         </StyledTabNavRoot>
